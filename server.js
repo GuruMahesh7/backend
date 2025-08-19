33b4958b314e5bca482943a7c3d3f4cc6aca91d0
Guru Mahesh <gurumaheshbusani@gmail.com>
2025-08-19 12:04:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // ✅ CORS Setup
 app.use(
   cors({
-    origin: "https://backend-1-9gjf.onrender.com", // Allow frontend origin
+    origin: "https://react-kft3.vercel.app/", // Allow frontend origin
     credentials: true, // Optional: if you're using cookies or auth
   })
 );
